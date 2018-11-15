9295bcf9dd3b7be01e6a19da52f0aa3523df7b5b
Robin Broda <robin@broda.me>
2018-11-15 11:06:21 +0100

    Main: Read cartridge

--- src/crestal-myth.cr
+++ src/crestal-myth.cr
@@ -1,6 +1,17 @@
+require "./lib/log"
+require "./lib/cartridge"
+
 # TODO: Write documentation for `Crestal::Myth`
 module Crestal::Myth
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
+  log = Log.new
+  log.info("Starting Crestal::Myth")
+
+  cart_path = "Tetris.gb" # TODO: Use app arguments
+  log.debug("Reading #{cart_path} as cartridge")
+  cart = Cartridge.new cart_path
+  log.info("Cartridge name: #{cart.name}")
+  log.debug("Cartridge code: #{cart.code}")
+  log.debug("Cartridge type: #{cart.type}")
 end

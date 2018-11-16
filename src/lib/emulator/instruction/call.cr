f67c617845672f5591c581d4a15f092ae83d5878
Robin Broda <robin@broda.me>
2018-11-16 20:22:45 +0100

    Emulator::Instruction::CALL: Use flag_read instead of manually reading bit

--- src/lib/emulator/instruction/call.cr
+++ src/lib/emulator/instruction/call.cr
@@ -15,13 +15,13 @@ module Crestal::Myth::Emulator::Instruction
         when DirectValue::SHORT
           jmp = cpu.ram.read16 cpu.reg.read Component::Reg16::PC
         when Conditional::NZ
-          cond = !cpu.reg.read(Component::Reg8::F).bit(7)
+          cond = !cpu.reg.flag_read(Component::Flag::Z)
         when Conditional::Z
-          cond = cpu.reg.read(Component::Reg8::F).bit(7)
+          cond = cpu.reg.flag_read(Component::Flag::Z)
         when Conditional::NC
-          cond = !cpu.reg.read(Component::Reg8::F).bit(4)
+          cond = !cpu.reg.flag_read(Component::Flag::C)
         when Conditional::C
-          cond = cpu.reg.read(Component::Reg8::F).bit(4)
+          cond = cpu.reg.flag_read(Component::Flag::C)
         else
           Log.new.fatal "Unimplemented #{disasm cpu}"
         end

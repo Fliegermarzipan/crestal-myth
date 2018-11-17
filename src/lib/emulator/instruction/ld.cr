0476618d7f1e77ac4faba219ed0167ac453dec06
Robin Broda <robin@broda.me>
2018-11-17 14:26:05 +0100

    Emulator::Instruction::LD: Fix load to byte

--- src/lib/emulator/instruction/ld.cr
+++ src/lib/emulator/instruction/ld.cr
@@ -13,10 +13,10 @@ module Crestal::Myth::Emulator::Instruction
         target = @args[0].as(Component::Reg8 | Component::Reg16)
       when Component::Reg8Mem
         target_reg = false
-        target = 0xFF00_u16 + cpu.ram.read(cpu.reg.read(Component::Reg8.new(@args[0].as(Component::Reg8Mem).value)))
+        target = 0xFF00_u16 + cpu.reg.read(Component::Reg8.new(@args[0].as(Component::Reg8Mem).value))
       when Component::Reg16Mem
         target_reg = false
-        target = cpu.ram.read16(cpu.reg.read(Component::Reg16.new(@args[0].as(Component::Reg16Mem).value)))
+        target = cpu.reg.read(Component::Reg16.new(@args[0].as(Component::Reg16Mem).value))
       when DirectValueMem::SHORT
         target_reg = false
         target = cpu.ram.read16 cpu.reg.read Component::Reg16::PC
@@ -26,7 +26,7 @@ module Crestal::Myth::Emulator::Instruction
         target = 0xFF00_u16 + cpu.ram.read cpu.reg.read Component::Reg16::PC
         cpu.reg.inc Component::Reg16::PC
       else
-        Log.new.fatal "Unimplemented #{disasm cpu}"
+        Log.new.fatal "Unimplemented '#{disasm cpu}' with #{@args[0]} in first arg"
         return false
       end
 
@@ -34,9 +34,9 @@ module Crestal::Myth::Emulator::Instruction
       when Component::Reg8, Component::Reg16
         value = cpu.reg.read @args[1].as(Component::Reg8 | Component::Reg16)
       when Component::Reg8Mem
-        value = 0xFF00_u16 + cpu.ram.read(cpu.reg.read(Component::Reg8.new(@args[1].as(Component::Reg8Mem).value)))
+        value = cpu.ram.read(0xFF00_u16 + cpu.reg.read(Component::Reg8.new(@args[1].as(Component::Reg8Mem).value)))
       when Component::Reg16Mem
-        value = cpu.ram.read16(cpu.reg.read(Component::Reg16.new(@args[1].as(Component::Reg16Mem).value)))
+        value = cpu.ram.read(cpu.reg.read(Component::Reg16.new(@args[1].as(Component::Reg16Mem).value)))
       when DirectValue::SHORT
         value = cpu.ram.read16 cpu.reg.read Component::Reg16::PC
         cpu.reg.inc Component::Reg16::PC, 2
@@ -44,7 +44,7 @@ module Crestal::Myth::Emulator::Instruction
         value = cpu.ram.read cpu.reg.read Component::Reg16::PC
         cpu.reg.inc Component::Reg16::PC
       else
-        Log.new.fatal "Unimplemented #{disasm cpu}"
+        Log.new.fatal "Unimplemented '#{disasm cpu}' with #{@args[1]} in second arg"
         return false
       end
 

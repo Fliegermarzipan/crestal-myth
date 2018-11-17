b72d18644dde33aa668e3363a356db5570599aec
Robin Broda <robin@broda.me>
2018-11-17 21:07:35 +0100

    Emulator::Instruction::LD: Fix load to Reg8

--- src/lib/emulator/instruction/ld.cr
+++ src/lib/emulator/instruction/ld.cr
@@ -4,13 +4,18 @@ module Crestal::Myth::Emulator::Instruction
   class LD < Opcode
     def call(cpu) : Bool
       target_reg = true
+      target_reg8 = true
       target = Component::Reg8::Placeholder
       value = 0_u16
 
       case @args[0]
-      when Component::Reg8, Component::Reg16
+      when Component::Reg8
         target_reg = true
-        target = @args[0].as(Component::Reg8 | Component::Reg16)
+        target = @args[0].as(Component::Reg8)
+      when Component::Reg16
+        target_reg = true
+        target_reg8 = false
+        target = @args[0].as(Component::Reg16)
       when Component::Reg8Mem
         target_reg = false
         target = 0xFF00_u16 + cpu.reg.read(Component::Reg8.new(@args[0].as(Component::Reg8Mem).value))
@@ -34,11 +39,11 @@ module Crestal::Myth::Emulator::Instruction
       when Component::Reg8, Component::Reg16
         value = cpu.reg.read @args[1].as(Component::Reg8 | Component::Reg16)
       when Component::Reg8Mem
-        value = cpu.ram.read(0xFF00_u16 + cpu.reg.read(Component::Reg8.new(@args[1].as(Component::Reg8Mem).value)))
+        value = cpu.ram.read(cpu.reg.read(Component::Reg8.new(@args[1].as(Component::Reg8Mem).value)))
       when Component::Reg16Mem
         value = cpu.ram.read(cpu.reg.read(Component::Reg16.new(@args[1].as(Component::Reg16Mem).value)))
       when DirectValueMem::SHORT
-        value = cpu.ram.read16 cpu.ram.read16 cpu.reg.read Component::Reg16::PC
+        value = cpu.ram.read cpu.ram.read16 cpu.reg.read Component::Reg16::PC
         cpu.reg.inc Component::Reg16::PC, 2
       when DirectValueMem::BYTE
         value = cpu.ram.read cpu.ram.read cpu.reg.read Component::Reg16::PC
@@ -55,9 +60,9 @@ module Crestal::Myth::Emulator::Instruction
       end
 
       if target_reg
-        if target == Component::Reg8
+        if target_reg8
           cpu.reg.write target.as(Component::Reg8), value.to_u8
-        elsif target == Component::Reg16
+        else
           cpu.reg.write target.as(Component::Reg16), value.as(UInt16)
         end
       else

cddb8f4cf7950770caf84e1561ec478970ffd46c
Robin Broda <robin@broda.me>
2018-11-17 14:30:35 +0100

    Add Emulator::Instruction::JR

---
+++ src/lib/emulator/instruction/jr.cr
@@ -0,0 +1,32 @@
+require "../opcode"
+
+module Crestal::Myth::Emulator::Instruction
+  class JR < Opcode
+    def call(cpu) : Bool
+      cond = true
+      jmp = 0x0000_i16
+
+      @args.each do |arg|
+        case arg
+        when DirectValue::BYTE
+          jmp = cpu.ram.read(cpu.reg.read Component::Reg16::PC).to_i8
+          cpu.reg.inc Component::Reg16::PC
+        when Conditional::NZ
+          cond = !cpu.reg.flag_read(Component::Flag::Z)
+        when Conditional::Z
+          cond = cpu.reg.flag_read(Component::Flag::Z)
+        when Conditional::NC
+          cond = !cpu.reg.flag_read(Component::Flag::C)
+        when Conditional::C
+          cond = cpu.reg.flag_read(Component::Flag::C)
+        else
+          Log.new.fatal "Unimplemented #{disasm cpu}"
+          return false
+        end
+      end
+
+      cpu.reg.write Component::Reg16::PC, cpu.reg.read(Component::Reg16::PC) + jmp if cond
+      true
+    end
+  end
+end
